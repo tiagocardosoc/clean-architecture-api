2eda3a5bc671335619ef6372218c6b5eda53ec59
tcardosoc <tcardosoc.dev@gmail.com>
2023-03-27 10:33:23 -0300

    feat: add class validator middleware (not working/useful yet)

---
+++ src/@Infra/middlewares/class-validator.middleware.ts
@@ -0,0 +1,19 @@
+
+import { Request, Response } from "express"
+import { validate, ValidationError } from "class-validator"
+import ContactCreateDTO from "../../@Domain/dtos/inputDTOs/contacts/InputContactCreateDTO"
+
+export const validateInput = async (req: Request, res: Response, next) => {
+    const { name, email, phoneNumber } = req.body;
+    const dto = new ContactCreateDTO(name, email, phoneNumber)
+    const details: ValidationError[] = await validate(dto)
+    if (details.length) {
+        res.status(500).json({
+            errors: details
+        })
+    }
+    console.log("validateInput FIRED!!", req.body, details)
+
+    next()
+
+}
