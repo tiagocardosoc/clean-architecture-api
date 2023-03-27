1b7fc8c9cd3d11018cdd831db88bc2143686fdba
tcardosoc <tcardosoc.dev@gmail.com>
2023-03-27 10:22:25 -0300

    feat: created base interfaces

---
+++ src/@Core/interfaces/index.ts
@@ -0,0 +1,17 @@
+export interface IUseCase<InputType, OutputType> {
+    execute(data: InputType): Promise<OutputType>
+}
+
+export interface IRepo<EntityType> {
+    create(data: EntityType): Promise<EntityType>,
+    update(data: EntityType): Promise<boolean>,
+    findAll(): Promise<EntityType[]>,
+    findOne(where: Partial<EntityType>): Promise<Partial<EntityType>>,
+    remove(item: EntityType): Promise<boolean>
+}
+
+export interface IOutputDTOBase {
+    responseMessage: string,
+    responseStatusCode: number,
+    responsepayload?: any
+}
